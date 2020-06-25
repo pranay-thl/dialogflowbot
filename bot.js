3d0a327ff92deeaced8dc3620e309c3b3690db93
Pranay Pandey <pranay.pnd@gmail.com>
2020-06-26 01:50:54 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,43 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(projectId = 'obstacles-ltchjw') {
+    // A unique identifier for the given session
+    const sessionId = uuid.v4();
+
+    // Create a new session
+    let keyFileName = __dirname + "/key.json";
+    const sessionClient = new dialogflow.SessionsClient({keyFilename:keyFileName});
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                // The query to send to the dialogflow agent
+                text: 'who are you ?',
+                // The language used by the client (en-US)
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+        console.log(`  No intent matched.`);
+    }
+}
+
+runSample();
